2a4e16fe8129841cf1d94a3b5aca90969fc65090
kcbighuge <kcbighuge@gmail.com>
2018-02-05 16:53:04 -0800

    create circular path using previous path points

--- src/main.cpp
+++ src/main.cpp
@@ -242,8 +242,44 @@ int main() {
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+          	// TODO IN PROGRESS: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            double pos_x;
+            double pos_y;
+            double angle;
+            int path_size = previous_path_x.size();
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            // use previous path points
+            for (int i=0; i < path_size; i++) {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            // set current position
+            if (path_size == 0) {
+              pos_x = car_x;
+              pos_y = car_y;
+              angle = deg2rad(car_yaw);
+            } else {
+              pos_x = previous_path_x[path_size-1];
+              pos_y = previous_path_y[path_size-1];
+
+              double pos_x2 = previous_path_x[path_size-2];
+              double pos_y2 = previous_path_y[path_size-2];
+              angle = atan2(pos_y-pos_y2, pos_x-pos_x2);
+            }
+
+            double dist_inc = 0.5;
+            for (int i=0; i < 50-path_size; i++) {
+              //next_x_vals.push_back(car_x + (dist_inc) * cos(deg2rad(car_yaw)));
+              //next_y_vals.push_back(car_y + (dist_inc) * sin(deg2rad(car_yaw)));
+
+              next_x_vals.push_back(pos_x + (dist_inc) * cos(angle + (i+1) * (pi()/100)));
+              next_y_vals.push_back(pos_y + (dist_inc) * sin(angle + (i+1) * (pi()/10)));
+
+              pos_x += dist_inc * cos(angle + (i+1) * (pi()/100));
+              pos_y += dist_inc * sin(angle + (i+1) * (pi()/100));
+            }
+
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
